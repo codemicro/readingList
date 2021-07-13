c7fe670c92889902b106211899e3eafadcd41ff7
AKP <tom@tdpain.net>
2021-07-13 17:59:33 +0100

    Add site generation code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/codemicro/readingList
+
+go 1.16
+
+require (
+	github.com/jszwec/csvutil v1.5.0 // indirect
+	github.com/stevelacy/daz v0.1.4
+)
---
+++ magefile.go
@@ -0,0 +1,188 @@
+// +build mage
+
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"text/template"
+	"time"
+
+	"github.com/jszwec/csvutil"
+	"github.com/stevelacy/daz"
+)
+
+const dateFormat = "2006-01-02"
+
+type readingListEntry struct {
+	URL         string    `csv:"url,omitempty"`
+	Title       string    `csv:"title,omitempty"`
+	Description string    `csv:"description,omitempty"`
+	Date        time.Time `csv:"date,omitempty"`
+}
+
+// renderAnchor renders a HTML anchor tag
+func renderAnchor(text, url string, newTab bool) daz.HTML {
+	attrs := daz.Attr{
+		"href": url,
+		"rel":  "noopener",
+	}
+	if newTab {
+		attrs["target"] = "_blank"
+	}
+	return daz.H("a", attrs, text)
+}
+
+func renderUnsafeAnchor(text, url string, newTab bool) daz.HTML {
+	attrs := daz.Attr{
+		"href": url,
+		"rel":  "noopener",
+	}
+	if newTab {
+		attrs["target"] = "_blank"
+	}
+	return daz.H("a", attrs, daz.UnsafeContent(text))
+}
+
+//go:embed page.template.html
+var htmlPageTemplate []byte
+
+// renderHTMLPage renders a complete HTML page
+func renderHTMLPage(title, titleBar, pageContent, extraHeadeContent string) ([]byte, error) {
+
+	tpl, err := template.New("page").Parse(string(htmlPageTemplate))
+	if err != nil {
+		return nil, err
+	}
+	outputBuf := new(bytes.Buffer)
+
+	tpl.Execute(outputBuf, struct {
+		Title            string
+		Content          string
+		PageTitleBar     string
+		ExtraHeadContent string
+	}{Content: pageContent, PageTitleBar: titleBar, Title: title, ExtraHeadContent: extraHeadeContent})
+
+	return outputBuf.Bytes(), nil
+}
+
+type entryGroup struct {
+	Date    time.Time
+	Entries []*readingListEntry
+}
+
+type entryGroupSlice []*entryGroup
+
+func (e entryGroupSlice) Len() int {
+	return len(e)
+}
+
+func (e entryGroupSlice) Less(i, j int) bool {
+	return e[i].Date.After(e[j].Date)
+}
+
+func (e entryGroupSlice) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+func groupEntriesByMonth(entries []*readingListEntry) entryGroupSlice {
+	groupMap := make(map[time.Time]*entryGroup)
+
+	for _, entry := range entries {
+		newTime := time.Date(entry.Date.Year(), entry.Date.Month(), 1, 0, 0, 0, 0, time.UTC)
+		if groupMap[newTime] == nil {
+			groupMap[newTime] = &entryGroup{
+				Date: newTime,
+			}
+		}
+		groupMap[newTime].Entries = append(groupMap[newTime].Entries, entry)
+	}
+
+	var o entryGroupSlice
+	for _, group := range groupMap {
+		o = append(o, group)
+	}
+	sort.Sort(o)
+
+	return o
+}
+
+// makeTILHTML generates HTML from a []*entryGroup to make a list of articles
+func makeListHTML(groups []*entryGroup) string {
+
+	const headerLevel = "h3"
+
+	var parts []interface{}
+	for _, group := range groups {
+
+		header := daz.H(headerLevel, fmt.Sprintf("%s %d", group.Date.Month().String(), group.Date.Year()))
+
+		var entries []daz.HTML
+		for _, article := range group.Entries {
+			items := []interface{}{renderAnchor(article.Title, article.URL, false), " - "+article.Date.Format(dateFormat)}
+			if article.Description != "" {
+				items = append(items, daz.H("br"), article.Description)
+			}
+			x := daz.H("li", items...)
+			entries = append(entries, x)
+		}
+
+		parts = append(parts, []daz.HTML{header, daz.H("ul", entries)})
+	}
+
+	return daz.H("div", parts...)()
+}
+
+func GenerateSite() error {
+
+	const outputDir = ".site"
+	const readingListFile = "readingList.csv"
+
+	// read CSV file
+	var entries []*readingListEntry
+
+	fcont, err := ioutil.ReadFile(readingListFile)
+	if err != nil {
+		return err
+	}
+
+	err = csvutil.Unmarshal(fcont, &entries)
+	if err != nil {
+		return err
+	}
+
+	numArticles := len(entries)
+	groupedEntries := groupEntriesByMonth(entries)
+
+	const pageTitle = "akp's reading list"	
+
+	head := daz.H(
+		"div",
+		daz.H("h1", pageTitle),
+		daz.H(
+			"p",
+			daz.UnsafeContent(
+				fmt.Sprintf(
+					"A mostly complete list of articles I've read<br>There are currently %d entries in the list<br>Last modified %s<br>Repo: %s",
+					numArticles,
+					time.Now().Format(dateFormat),
+					renderUnsafeAnchor("<code>codemicro/readingList</code>", "https://github.com/codemicro/readingList", false)(),
+				),
+			),
+		),
+	)
+
+	listingHTML := makeListHTML(groupedEntries)
+
+	outputContent, err := renderHTMLPage(pageTitle, head(), listingHTML, "")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(outputContent))
+
+	return nil
+}

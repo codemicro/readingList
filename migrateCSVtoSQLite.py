60dbcab706f851fd77a215b1675064ccc5bd2f50
AKP <abi@tdpain.net>
2024-05-27 14:56:22 +0100

    Stop using NULL in the database
    It's simply easier not to with the stock SQL engine

--- migrateCSVtoSQLite.py
+++ migrateCSVtoSQLite.py
@@ -10,7 +10,7 @@ SQLITEFILE = sys.argv[2]
 with open(CSVFILE) as csvf:
     csv_reader = csv.reader(csvf)
 
-    x = list(map(lambda x: (str(uuid.uuid4()), *map(lambda y: (y if y != "" else None), x)), list(csv_reader)[1:]))
+    x = list(map(lambda x: (str(uuid.uuid4()), *map(lambda y: (y if y != "" else ""), x)), list(csv_reader)[1:]))
     db = sqlite3.connect(SQLITEFILE)
     db.executemany(
         """INSERT INTO articles("id", "url", "title", "description", "image_url", "date", "hacker_news_url") VALUES (?, ?, ?, ?, ?, ?, ?)""",
--- models/article.go
+++ models/article.go
@@ -1,20 +1,20 @@
 package models
 
 import (
-	"time"
 	"github.com/google/uuid"
+	"time"
 )
 
 type NewArticle struct {
-	URL           string `validate:"required,url"`
-	Title         string `validate:"required"`
-	Description   string `db:"description,nullzero"`
-	ImageURL      string `db:"image_url,nullzero"`
-	Date          time.Time `validate:"required"`
+	URL         string    `validate:"required,url"`
+	Title       string    `validate:"required"`
+	Description string    `db:"description"`
+	ImageURL    string    `db:"image_url"`
+	Date        time.Time `validate:"required"`
 }
 
 type Article struct {
 	NewArticle
 	ID            uuid.UUID
-	HackerNewsURL string `db:"hacker_news_url,nullzero"`
+	HackerNewsURL string `db:"hacker_news_url"`
 }
